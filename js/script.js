4a722a2ff07a2aefb6e417737d6824e7fc6a60b9
Cynthia Castillo <ca.castillo@gmail.com>
2022-07-25 15:00:26 -0500

    More functionality js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,38 @@ function originalColor(aTag){
 function returnTitle(element) {
     element.innerText = "Using Abstract";
 }
+
+function changeH2() {
+    var elementH2 = document.querySelector("h2"); 
+    elementH2.innerText = "How can we help you, Elena?";
+
+    var submitButton = document.querySelector("#submit");
+    submitButton.style.background = "blue";
+}
+
+function switchClasses() {
+    var helpSpan = document.querySelector("#help-text");
+
+    if(helpSpan.classList.contains('italic')) { /*I'm asking if the helpSpan has italic class */
+        helpSpan.classList.remove('italic');
+    } else {
+        helpSpan.classList.add('italic');
+    }
+
+    //helpSpan.classList.add('italic');
+}
+
+function searching(inputSearch) {
+    /* inputSearch = '<input id="search" type="text" placeholder="Search" onchange="searching(this)">' */
+    var valueInput = inputSearch.value; /* valueInput = "buttons" */
+    var spanLookingFor = document.querySelector('#looking-for'); 
+    /* spanLookingFor = '<span id="looking-for"></span>' */
+    spanLookingFor.innerText = valueInput;
+
+    /*
+    var idElement = document.querySelector('#id');
+    var classElement = document.querySelector('.class');
+    var element = document.querySelector('element');
+    */
+
+}
